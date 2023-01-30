cf5d35e0805e4e1441ece111c89473b83076a025
Germani Benítez Alfonso <germanibenitezalfonso1@gmail.com>
2023-01-30 17:32:15 -0500

    second

---
+++ src/Navegacion.js
@@ -0,0 +1,10 @@
+function Navegacion(params) {
+    return <div>
+        <h2>Restaurante Tamarindo</h2>
+        <nav>
+            <a href="">Tamarindo RESTAURANTE MEXICANO TAMARINDO, A CORUÑA</a>
+        </nav>
+    </div>
+}
+
+export default Navegacion
